ad31221fa261b69e335a36abf11c8c7d2212a6fc
Krish <krish.r@elaunchinfotech.in>
2024-07-30 17:24:44 +0530

    - port change

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import http from 'http';
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end('WebSocket server is running\n');
 });
 
