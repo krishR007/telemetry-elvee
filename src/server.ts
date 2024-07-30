98cf521d3af3f6eeef12b9533347c3345e92396e
Krish <krish.r@elaunchinfotech.in>
2024-07-30 17:15:37 +0530

    - port change

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+import https from 'https';
+import fs from 'fs';
 
-// Create an HTTP server
-const server = http.createServer((req, res) => {
+// SSL certificate and key files
+const options = {
+    cert: fs.readFileSync('certs/0000_cert.pem'),
+    key: fs.readFileSync('certs/privkey.pem'),
+    ca: fs.readFileSync('certs/0001_chain.pem')
+};
+
+// Create an HTTPS server
+const server = https.createServer(options, (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('WebSocket server is running\n');
+    res.end('WebSocket server is running with SSL\n');
 });
 
 // Create a WebSocket server
