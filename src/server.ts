919efbd09a8bb446ce729030717bbb6b9b0ad639
Krish <krish.r@elaunchinfotech.in>
2024-08-06 09:57:40 +0530

    - changes

--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,42 @@
+import WebSocket, {WebSocketServer} from 'ws';
 import http from 'http';
-import {Server} from 'socket.io';
-import {io as ClientSocketIO} from 'socket.io-client';
 
-// Create an HTTP server
-const httpServer = http.createServer();
-
-// Create a Socket.IO server
-const io = new Server(httpServer, {
-    cors: {
-        origin: "*", // You might want to restrict this in a production environment
-    }
+const server = http.createServer((req, res) => {
+    console.log(req);
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({message: 'WebSocket server is running'}));
 });
 
-// Handle connection event
-io.on('connection', (socket) => {
-    console.log('A client connected');
+const wss = new WebSocketServer({server});
 
-    // Handle custom events
-    socket.on('customEvent', (data) => {
-        console.log('Received custom event with data:', data);
+wss.on('connection', (ws: WebSocket) => {
+    console.log('A new client connected!');
+
+    ws.on('message', (data: any, isBinary: boolean) => {
+        console.log(typeof data)
+        if (isBinary) {
+            const enc = new TextDecoder("utf-8");
+            const arr = new Uint8Array(data);
+            console.log(data);
+            console.log(isBinary);
+            console.log(enc.decode(arr));
+        } else {
+            console.log("Not binary")
+        }
     });
 
-    // Handle disconnection event
-    socket.on('disconnect', () => {
-        console.log('A client disconnected');
+
+    ws.on('error', (error) => {
+        console.error('WebSocket error:', error);
     });
 
-    // Handle errors
-    socket.on('error', (error) => {
-        console.error('Socket error:', error);
+    ws.on('close', () => {
+        console.log('Client disconnected');
     });
 });
 
-// Connect to the Socket.IO server
-const clientSocket = ClientSocketIO('https://telemetry.elvee.app');
-
-clientSocket.on('connect', () => {
-    console.log('Connected to Socket.IO server');
-});
-
-clientSocket.on('message', (data) => {
-    console.log('Received message from Socket.IO server:', data);
-});
-
-clientSocket.on('disconnect', () => {
-    console.log('Disconnected from Socket.IO server');
-});
-
-clientSocket.on('error', (error) => {
-    console.error('Socket.IO client error:', error);
-});
-
-// Start listening on port 8080
-httpServer.listen(8080, () => {
-    console.log('Server is listening on port 8080');
+// Start the server
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
